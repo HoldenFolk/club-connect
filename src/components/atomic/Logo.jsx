da79042180769ba8665c30374348e484a5a46ad6
unknown <holdenfolk1@gmail.com>
2024-12-03 15:24:26 -0500

    Add sidebar component for dashboard

--- src/components/atomic/Logo.jsx
+++ src/components/atomic/Logo.jsx
@@ -3,11 +3,11 @@ import logo_white from '../../assets/logo_white.png'
 import logo_black from '../../assets/logo_black.png'
 
 // Custom logo component with a white and black variant
-const Logo = ({variant = 'white', ...props}) => {
+const Logo = ({variant = 'white', height=80, ...props}) => {
     const src = (variant === 'white') ? logo_white : logo_black;
 
     return (
-        <img src={src} alt='My Clubs Logo' {...props} />
+        <img src={src} alt='My Clubs Logo' height={height} {...props} />
     )
 };
 
--- src/components/organism/screens/Dashboard.jsx
+++ src/components/organism/screens/Dashboard.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import { PageTemplate } from '../../template/index';
-import backgroundImage from '../../../assets/backsplash.png';
-import LandingTitle from '../../molecule/LandingTitle';
-import Button from '../../atomic/Button';
-import LandingPageHeader from '../../molecule/LandingPageHeader';
+import VerticalSidebar from '../../molecule/VerticalSidebar';
+
 
 // Ladning Page Screen Component
-// TODO: Test styles on diffent devices
-// TOOD: Modularize into components?
 export const Dashboard = () => {
 	return (
 		<PageTemplate>
-
+			<VerticalSidebar/>
 		</PageTemplate>
 		
 	);
